f481d11a77691778a40d17307d46c39c85b64e2a
Igor <des.yogi@gmail.com>
2024-01-10 20:37:10 +0200

    article in progress

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -62,3 +62,14 @@ if(~navigator.appVersion.indexOf("Linux"))cth('linux');
     // Your custom options
   });
 }());
+
+(function(){
+  const phoneElems = document.getElementsByClassName('phone-mask');
+  Array.prototype.forEach.call(phoneElems, function (item) {
+    const phoneMask = IMask(
+      item, {
+        mask: '+{38} (\\000) 000 00 00',
+        lazy: false // make placeholder always visible
+    });
+  });
+}());
